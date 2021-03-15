860083c8a9d3dd3bf63645b874e50e885a0c4759
unknown <idestura35@gmail.com>
2021-03-15 22:54:31 +0800

    leadership

--- assets/dist/leadership.js
+++ assets/dist/leadership.js
@@ -1,22 +1,29 @@
 $(document).ready(function () {
 
     var storedScores = JSON.parse(localStorage.getItem("Scores"));
-
     
-    storedScores.sort(function (a, b) { 
-        return b.score - a.score;
-    })
-
-    var html = ''
-
-    storedScores.forEach((element, index) => {
-        if(index <= 3){
-            html += '<li style="list-style:none; font-size: 30px">' + element.user + ' - ' + element.score + '%</li>';
-            return false;
-        }
-    });
-
-    $('.list').html(html);
+    if(storedScores!=null){
+        storedScores.sort(function (a, b) { 
+            return b.score - a.score;
+        })
+    
+        var html = ''
+    
+        storedScores.forEach((element, index) => {
+            if(index <= 3){
+                html += '<li style="list-style:none; font-size: 30px">' + element.user + ' - ' + element.score + '%</li>';
+                return false;
+            }
+        });
+    
+        $('.list').html(html);
+    }else{
+        var html = ''
+    
+        html += '<li style="list-style:none; font-size: 30px">No User Yet</li>';
+    
+        $('.list').html(html);
+    }
 
 
     $('.play-btn').on('click', function () {
